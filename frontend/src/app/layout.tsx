ee97b6e9bebc106a6a46d46f681aeadfe0d09e8a
Guillermo Perez <gperez78@gmail.com>
2024-06-04 01:25:45 -0300

    Install MUI

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,8 +1,7 @@
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
+import { ThemeProvider } from "@mui/material/styles";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
+import theme from "../theme";
 
 export const metadata: Metadata = {
   title: "Number8 assesment",
@@ -16,7 +15,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>
+        <AppRouterCacheProvider>
+          <ThemeProvider theme={theme}>{children}</ThemeProvider>
+        </AppRouterCacheProvider>
+      </body>
     </html>
   );
 }
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,10 @@
+import Button from "@mui/material/Button";
 import styles from "./page.module.css";
 
 export default function Home() {
-  return <main className={styles.main}>...</main>;
+  return (
+    <main className={styles.main}>
+      <Button variant="contained">Add Employee</Button>
+    </main>
+  );
 }
