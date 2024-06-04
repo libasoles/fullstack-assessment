5486a9b7e9c96d86b9e4614f4786488c0558dc9b
Guillermo Perez <gperez78@gmail.com>
2024-06-04 20:39:59 -0300

    Add inactive label

--- frontend/src/components/EmployeeAvatar.tsx
+++ frontend/src/components/EmployeeAvatar.tsx
@@ -1,6 +1,8 @@
 import Avatar, { AvatarProps } from "@mui/material/Avatar";
 import Badge from "@mui/material/Badge";
 import Box from "@mui/material/Box";
+import Stack from "@mui/material/Stack";
+import Typography from "@mui/material/Typography";
 
 interface Props extends AvatarProps {
   isDeactivated?: boolean;
@@ -13,15 +15,20 @@ export const EmployeeAvatar = ({ isDeactivated = false, ...rest }: Props) => {
   if (!isDeactivated) return avatar;
 
   return (
-    <Box>
-      <Badge
-        overlap="circular"
-        anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
-        color="error"
-        badgeContent=""
-      >
-        {avatar}
-      </Badge>
-    </Box>
+    <Stack spacing={0.5}>
+      <Box>
+        <Badge
+          overlap="circular"
+          anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+          color="error"
+          badgeContent=""
+        >
+          {avatar}
+        </Badge>
+      </Box>
+      <Typography color="text.secondary" variant="caption">
+        Inactive
+      </Typography>
+    </Stack>
   );
 };
