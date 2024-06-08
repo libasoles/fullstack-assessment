defac48efe3910ead17bf5b90de9d2923a28e574
Guillermo Perez <gperez78@gmail.com>
2024-06-07 21:20:10 -0300

    Keep departments in cache

--- frontend/src/api/useFetchDepartments.ts
+++ frontend/src/api/useFetchDepartments.ts
@@ -10,12 +10,10 @@ async function fetchDepartments() {
   return response.data;
 }
 
-const oneHour = 1000 * 60 * 60;
-
 export function useFetchDepartments(): UseQueryResult<Department[]> {
   return useQuery({
     queryKey: [DEPARTMENTS],
     queryFn: fetchDepartments,
-    staleTime: oneHour, // TODO: figure out why it only works on a single page, even though we navigate with Next Link
+    staleTime: Infinity,
   });
 }
